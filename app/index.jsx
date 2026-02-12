10b450eb530342b66d121687051263962d0b7207
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-02-12 11:09:24 -0300

    Estilizando a tela inicial

---
+++ app/index.jsx
@@ -0,0 +1,69 @@
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("./pomodoro.png")} />
+      <Text style={styles.text}>Edit app/index.jsx to edit this screen!!!</Text>
+      <View style={styles.actions}>
+        <Text style={styles.timer}>25:00</Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Começar</Text>
+        </Pressable>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Projeto fictício e sem fins comerciais.
+        </Text>
+        <Text style={styles.footerText}>Desenvolvido por Alura.</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#021123",
+    gap: 40,
+  },
+  text: {
+    color: "#FFF",
+  },
+  actions: {
+    paddingVertical: 24,
+    paddingHorizontal: 24,
+    backgroundColor: "#14448080",
+    width: "80%",
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: "#144480",
+    gap: 32,
+  },
+  timer: {
+    fontSize: 54,
+    color: "#FFF",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#B872FF",
+    borderRadius: 32,
+    padding: 10,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "#021123",
+    fontSize: 18,
+  },
+  footer: {
+    width: "80%",
+  },
+  footerText: {
+    textAlign: "center",
+    color: "#98A0A8",
+    fontSize: 12.5,
+  },
+});
