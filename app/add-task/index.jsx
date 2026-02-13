6f1e1a37f3652720af2babdb757352bede902192
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-02-13 15:48:02 -0300

    Estilizando o forms de tarefa

--- app/add-task/index.jsx
+++ app/add-task/index.jsx
@@ -1,10 +1,60 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView, Text, View, TextInput, Pressable, StyleSheet } from "react-native";
+import { IconSave } from "../../components/Icons";
 
-export default function AddTask() {
-  return (
-    <SafeAreaView>
-      <Text>Adicionar uma tarefa</Text>
-    </SafeAreaView>
-  );
+export default function AddTask () {
+    return (<SafeAreaView style={styles.container}>
+        <Text style={styles.text}>
+            Adicionar uma tarefa:
+        </Text>
+        <View style={styles.inner}>
+            <Text style={styles.label}>
+                Em que você está trabalhando?
+            </Text>
+            <TextInput 
+                style={styles.input}
+                numberOfLines={10}    
+            />
+            <Pressable style={styles.button}>
+                <IconSave /> 
+                <Text>
+                    Salvar
+                </Text>
+            </Pressable>
+        </View>
+    </SafeAreaView>)
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#021123',
+        gap: 16,
+        alignItems: 'center'
+    },
+    text: {
+        color: '#FFF',
+        textAlign: 'center',
+        fontSize: 26
+    },
+    inner: {
+        backgroundColor: '#98A0A8',
+        width: '90%',
+        borderRadius: 8,
+        padding: 16,
+        gap: 32
+    },
+    label: {
+        fontWeight: 600,
+        fontSize: 18
+    },
+    input: {
+        backgroundColor: '#fff',
+        padding: 16,
+        borderRadius: 8
+    },
+    button: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 4
+    }
+})
--- app/tasks/index.jsx
+++ app/tasks/index.jsx
@@ -2,47 +2,44 @@ import { StyleSheet, Text, View } from "react-native";
 import TaskItem from "../../components/TaskItem";
 import { FokusButton } from "../../components/FokusButton";
 import { IconPlus } from "../../components/Icons";
+import { router } from "expo-router";
 
 export default function Tasks() {
-    return (<View style={styles.container}>
-        <View style={styles.wrapper} >
-            <Text style={styles.text}>
-                Lista de tarefas:
-            </Text>
-            <View style={styles.inner}>
-                <TaskItem
-                    completed
-                    text="Estudar React"
-                />
-                <TaskItem
-                    text="Estudar React Native"
-                />
-            </View>
-            <FokusButton
-                title="Adicionar nova tarefa"
-                icon={<IconPlus outline />}
-                outline
-            />
+  return (
+    <View style={styles.container}>
+      <View style={styles.wrapper}>
+        <Text style={styles.text}>Lista de tarefas:</Text>
+        <View style={styles.inner}>
+          <TaskItem completed text="Estudar React" />
+          <TaskItem text="Estudar React Native" />
         </View>
-    </View>)
+        <FokusButton
+          title="Adicionar nova tarefa"
+          icon={<IconPlus outline />}
+          outline
+          onPress={() => router.navigate("/add-task")}
+        />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#021123',
-        alignItems: 'center'
-    },
-    wrapper: {
-        gap: 40,
-        width: '90%'
-    },
-    text: {
-        textAlign: 'center',
-        color: '#FFF',
-        fontSize: 26
-    },
-    inner: {
-        gap: 8
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#021123",
+    alignItems: "center",
+  },
+  wrapper: {
+    gap: 40,
+    width: "90%",
+  },
+  text: {
+    textAlign: "center",
+    color: "#FFF",
+    fontSize: 26,
+  },
+  inner: {
+    gap: 8,
+  },
+});
