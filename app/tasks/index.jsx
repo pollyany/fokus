e363e013122b139cfe6b3199c788ae450e657ccc
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-02-13 18:53:04 -0300

    Deletando tarefas

--- app/tasks/index.jsx
+++ app/tasks/index.jsx
@@ -6,55 +6,60 @@ import { router } from "expo-router";
 import useTaskContext from "../../components/context/useTaskContext";
 
 export default function Tasks() {
+  const { tasks, deleteTask } = useTaskContext();
 
-    const { tasks } = useTaskContext()
-
-    return (<View style={styles.container}>
-        <View style={styles.wrapper} >
-
-            <View style={styles.inner}>
-                <FlatList
-                    data={tasks}
-                    renderItem={({ item }) => <TaskItem
-                        completed={item.completed}
-                        text={item.description}
-                        key={item.id}
-                    />}
-                    keyExtractor={item => item.id}
-                    ItemSeparatorComponent={() => <View style={{ height: 8 }} />}
-                    ListHeaderComponent={<Text style={styles.text}>Lista de tarefas:</Text>}
-                    ListFooterComponent={<View style={{ marginTop: 16 }}>
-                        <FokusButton
-                            title="Adicionar nova tarefa"
-                            icon={<IconPlus outline />}
-                            outline
-                            onPress={() => router.navigate('/add-task')}
-                        />
-                    </View>}
+  return (
+    <View style={styles.container}>
+      <View style={styles.wrapper}>
+        <View style={styles.inner}>
+          <FlatList
+            data={tasks}
+            renderItem={({ item }) => (
+              <TaskItem
+                completed={item.completed}
+                text={item.description}
+                onPressDelete={() => deleteTask(item.id)}
+              />
+            )}
+            keyExtractor={(item) => item.id}
+            ItemSeparatorComponent={() => <View style={{ height: 8 }} />}
+            ListHeaderComponent={
+              <Text style={styles.text}>Lista de tarefas:</Text>
+            }
+            ListFooterComponent={
+              <View style={{ marginTop: 16 }}>
+                <FokusButton
+                  title="Adicionar nova tarefa"
+                  icon={<IconPlus outline />}
+                  outline
+                  onPress={() => router.navigate("/add-task")}
                 />
-            </View>
-
+              </View>
+            }
+          />
         </View>
-    </View>)
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#021123',
-        alignItems: 'center'
-    },
-    wrapper: {
-        gap: 40,
-        width: '90%'
-    },
-    text: {
-        textAlign: 'center',
-        color: '#FFF',
-        fontSize: 26,
-        marginBottom: 16
-    },
-    inner: {
-        gap: 8
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#021123",
+    alignItems: "center",
+  },
+  wrapper: {
+    gap: 40,
+    width: "90%",
+  },
+  text: {
+    textAlign: "center",
+    color: "#FFF",
+    fontSize: 26,
+    marginBottom: 16,
+  },
+  inner: {
+    gap: 8,
+  },
+});
