8b454e56615ebac0b51a69382117947902e10473
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-02-13 18:49:22 -0300

    Adicionando scroll

--- app/tasks/index.jsx
+++ app/tasks/index.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import TaskItem from "../../components/TaskItem";
 import { FokusButton } from "../../components/FokusButton";
 import { IconPlus } from "../../components/Icons";
@@ -11,26 +11,29 @@ export default function Tasks() {
 
     return (<View style={styles.container}>
         <View style={styles.wrapper} >
-            <Text style={styles.text}>
-                Lista de tarefas:
-            </Text>
+
             <View style={styles.inner}>
-                {tasks.map(t => {
-                    return (
-                        <TaskItem
-                            completed={t.completed}
-                            text={t.description}
-                            key={t.id}
+                <FlatList
+                    data={tasks}
+                    renderItem={({ item }) => <TaskItem
+                        completed={item.completed}
+                        text={item.description}
+                        key={item.id}
+                    />}
+                    keyExtractor={item => item.id}
+                    ItemSeparatorComponent={() => <View style={{ height: 8 }} />}
+                    ListHeaderComponent={<Text style={styles.text}>Lista de tarefas:</Text>}
+                    ListFooterComponent={<View style={{ marginTop: 16 }}>
+                        <FokusButton
+                            title="Adicionar nova tarefa"
+                            icon={<IconPlus outline />}
+                            outline
+                            onPress={() => router.navigate('/add-task')}
                         />
-                    )
-                })}
+                    </View>}
+                />
             </View>
-            <FokusButton
-                title="Adicionar nova tarefa"
-                icon={<IconPlus outline />}
-                outline
-                onPress={() => router.navigate('/add-task')}
-            />
+
         </View>
     </View>)
 }
@@ -48,7 +51,8 @@ const styles = StyleSheet.create({
     text: {
         textAlign: 'center',
         color: '#FFF',
-        fontSize: 26
+        fontSize: 26,
+        marginBottom: 16
     },
     inner: {
         gap: 8
